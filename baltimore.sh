e7e620db6caf8144b83de5b0977c6c9110f5f81e
Justin Gawrilow <justin.gawrilow@gmail.com>
2015-10-19 08:55:31 -0400

    Update baltimore.sh

--- baltimore.sh
+++ baltimore.sh
@@ -1,1 +1,1 @@
-python -u realtimegeo.py -key 0c2730831ff94174b515fb3c6caea0 -start_date 2015081600 -bb 39.236,-76.706,39.373,-76.528 -es http://10.1.94.103:9200/ -es_index baltimore --send_to_kafka -kafka memex-kafka01:9092
+python -u realtimegeo.py -key 0c2730831ff94174b515fb3c6caea0 -start_date 2015081600 -bb 39.236,-76.706,39.373,-76.528 -es http://10.1.94.103:9200/ -es_index baltimore
